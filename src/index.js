d2f122f45ccd8956f1c56de9c9004e04527b8b92
Dario <dario.fuzinato@meetrics.com>
2017-11-09 14:39:04 +0100

    v 8-9

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Route } from "react-router-dom";
 import './css/style.css'
 import StorePicker from './components/StorePicker'
 import App from './components/App'
+import NotFound from './components/NotFound'
 
-
-render(<App/>, document.querySelector('#main'));
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Route exact path="/" component={StorePicker} />
+        <Route path="/store/:storeId" component={App} />
+        <Route component={NotFound} />
+      </div>
+    </BrowserRouter>
+  )
+}
+render(<Root/>, document.querySelector('#main'));
