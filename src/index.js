ab49fcdec20e482a48f320ddf512b1b5aeb317e7
Dario <dario.fuzinato@meetrics.com>
2017-11-09 15:06:37 +0100

    v 10

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './css/style.css'
 import StorePicker from './components/StorePicker'
 import App from './components/App'
@@ -10,9 +10,11 @@ const Root = () => {
   return (
     <BrowserRouter>
       <div>
-        <Route exact path="/" component={StorePicker} />
-        <Route path="/store/:storeId" component={App} />
-        <Route component={NotFound} />
+        <Switch>
+          <Route exact path="/" component={StorePicker} />
+          <Route path="/store/:storeId" component={App} />
+          <Route component={NotFound} />
+        </Switch>
       </div>
     </BrowserRouter>
   )
