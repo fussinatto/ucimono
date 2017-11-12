c761e7515003a3d978c18f5e9a993f53480fb3f9
Dario <fussinatto@gmail.com>
2017-11-12 21:26:02 +0100

    v 18

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Order from './Order'
 import Inventory from './Inventory'
 import sampleFishes from '../sample-fishes'
 import Fish from './Fish'
+import base from '../base';
 
 class App extends React.Component {
   constructor () {
@@ -15,6 +16,16 @@ class App extends React.Component {
     this.addFish = this.addFish.bind(this)
     this.loadSamples = this.loadSamples.bind(this)
     this.addToOrder = this.addToOrder.bind(this)
+  }
+  componentWillMount () {
+    this.ref = base.syncState(`${this.props.match.params.storeId}/fishes`, {
+      context: this,
+      state: 'fishes'
+    })
+  }
+
+  componentWillUnmount () {
+    base.removeBinding(this.ref)
   }
   addFish (fish) {
     let fishes = {...this.state.fishes}
